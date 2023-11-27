c4f30342d78887650ad4062cc80990fbfd67efee
Prachi Gupta <117148255+prachiguptadev@users.noreply.github.com>
2023-11-27 17:26:54 +0000

    create a table

---
+++ index.js
@@ -0,0 +1,9 @@
+// create a mutiply table using do while
+
+let x = 5;  
+
+let i = 1;               //intilize
+do{
+  console.log(`${x} * ${i} = ${5*i}`)
+  i++;                  //increment the value of i
+}while(i <= 10)        //cheak condition
